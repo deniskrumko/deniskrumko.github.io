9051153a8b96ef92f66d19e65b4bd43e7483230e
Denis Krumko <krumko.d@saritasa.com>
2017-04-23 16:05:36 +0700

    Fixed animations on index page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(window).on("scroll", function() {
-    $("#hey").toggleClass("shrink", $(this).scrollTop() > 100)
-    $("#brand").toggleClass("shrink-brand", $(this).scrollTop() > 100)
-});
+    $("#navbar").toggleClass("shrink", $(this).scrollTop() > 100)
+    $(".nav-item").toggleClass("shrink-nav", $(this).scrollTop() > 100)
+  });
 });
