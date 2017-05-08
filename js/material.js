4255ad128857a0d0041244bc29c022e201a44432
Denis Krumko <krumko.d@saritasa.com>
2017-05-08 22:07:50 +0700

    Update material site

---
+++ js/material.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+    setTimeout(function(){
+      $('#email-me').addClass('scale-in');
+    }, 500);
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
