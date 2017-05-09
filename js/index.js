225df08e3191ac56d7675a3ffd62599707afbcac
Denis Krumko <krumko.d@saritasa.com>
2017-05-09 16:36:48 +0700

    Set material site version as a main version

---
+++ js/index.js
@@ -0,0 +1,22 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    });
+    $('.brand-logo').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    });
+
+    // setTimeout(function(){
+    //   $('#email-me').addClass('scale-in');
+    // }, 500);
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
--- js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $(window).on("scroll", function() {
-    $("#navbar").toggleClass("shrink", $(this).scrollTop() > 100)
-    $(".nav-item").toggleClass("shrink-nav", $(this).scrollTop() > 100)
-  });
-});
